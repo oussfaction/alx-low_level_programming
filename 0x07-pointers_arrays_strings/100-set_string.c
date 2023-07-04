fc91764bd99ee308ff167234ccff97e07cb602f9
oussfaction <oussfaction@gmail.com>
2023-07-04 02:43:30 -0700

    0.07

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,10 +2,11 @@
 
 /**
  * set-string - Entry point
- * s : input
- * to : input
+ * @s : input
+ * @to : input
  * Return: Always 0 (Success)
 */
+
 void set_string(char **s, char *to)
 {
 	*s = to;
