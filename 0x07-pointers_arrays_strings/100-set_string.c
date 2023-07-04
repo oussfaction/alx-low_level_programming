ba74bb07d49b95106caf51703a8c26606cbeafe1
oussfaction <oussfaction@gmail.com>
2023-07-04 03:30:07 -0700

    0.07

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
- * set-string - Entry point
- * @s : input
- * @to : input
- * Return: Always 0 (Success)
+ * set_string - Function to set the value of a pointer to a string
+ * @s: Pointer to a pointer to a string
+ * @to: Pointer to the string to set
 */
-
 void set_string(char **s, char *to)
 {
 	*s = to;
