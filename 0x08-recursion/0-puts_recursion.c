37c3d5d97e83c77377bde0ba11c620251509e61e
oussfaction <oussfaction@gmail.com>
2023-07-05 06:35:53 -0700

    0.08

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _puts_recursion - a function that prints a string, followed by a new lin
- * @n: input
+ * @s: input
  * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
