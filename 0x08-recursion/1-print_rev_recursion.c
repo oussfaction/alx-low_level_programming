60c0f85806c158373132eda40e9286fcc02492bb
oussfaction <oussfaction@gmail.com>
2023-07-05 06:46:40 -0700

    0.08

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * __print_rev_recursion -  prints a string in reverse
+ * _print_rev_recursion -  prints a string in reverse
  *@s: The string to print
  */
 void _print_rev_recursion(char *s)
